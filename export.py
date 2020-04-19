2ee12fc16bb3689c9a8756225c34b6226b4b9a58
773899206 <773899206@qq.com>
2020-04-19 18:45:24 +0100

    1

--- export.py
+++ export.py
@@ -12,6 +12,7 @@ import csv
 
 csvfile = open('a.csv', 'wb')
 writer = csv.writer(csvfile,dialect='excel')
+writer.writerow(['weather_main', 'weather_description', 'temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity', 'visibility','wind_speed', 'wind_deg', 'clouds_all', 'rain_1h', 'rain_3h', 'snow_1h', 'snow_3h', 'dt', 'sunrise', 'sunset'])
 conn,cur = utils.get_conn_cur()
 cur.execute("select * from availability")
 data=cur.fetchall
