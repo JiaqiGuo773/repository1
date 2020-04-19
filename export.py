baf2650d132bc921eb2296a9778dd65762a8a8a3
773899206 <773899206@qq.com>
2020-04-19 19:30:02 +0100

    1

--- export.py
+++ export.py
@@ -10,7 +10,7 @@ import pickle
 import utils
 import csv
 
-csvfile = open('a.csv', 'wb')
+csvfile = open('a.csv', 'w')
 writer = csv.writer(csvfile,dialect='excel')
 writer.writerow(['weather_main', 'weather_description', 'temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity', 'visibility','wind_speed', 'wind_deg', 'clouds_all', 'rain_1h', 'rain_3h', 'snow_1h', 'snow_3h', 'dt', 'sunrise', 'sunset'])
 conn,cur = utils.get_conn_cur()
