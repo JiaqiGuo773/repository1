7ebab95a394914721d96701b4ae248d5f1b7159b
773899206 <773899206@qq.com>
2020-04-19 19:42:25 +0100

    1

--- export.py
+++ export.py
@@ -10,15 +10,18 @@ import pickle
 import utils
 import csv
 
-csvfile = open('a.csv', 'w')
-writer = csv.writer(csvfile,dialect='excel')
-writer.writerow(['weather_main', 'weather_description', 'temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity', 'visibility','wind_speed', 'wind_deg', 'clouds_all', 'rain_1h', 'rain_3h', 'snow_1h', 'snow_3h', 'dt', 'sunrise', 'sunset'])
+#csvfile = open('a.csv', 'w')
+#writer = csv.writer(csvfile,dialect='excel')
+#writer.writerow(['weather_main', 'weather_description', 'temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity', 'visibility','wind_speed', 'wind_deg', 'clouds_all', 'rain_1h', 'rain_3h', 'snow_1h', 'snow_3h', 'dt', 'sunrise', 'sunset'])
 conn,cur = utils.get_conn_cur()
 cur.execute("select * from availability")
 data=cur.fetchall
-print(data)
-writer.writerows(data)
-csvfile.close()
-conn.commit()
 cur.close()
 conn.close()
+filename='b.csv'
+with open(filename,mode='w',encoding='utf-8') as f:
+        write = csv.writer(f,dialect='excel')
+        for item in data:
+            write.writerow(item)
+
+
