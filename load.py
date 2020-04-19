3fe99a3227ad110e2c70ef639975586b506a9169
773899206 <773899206@qq.com>
2020-04-19 17:49:39 +0100

    1

--- load.py
+++ load.py
@@ -18,17 +18,13 @@ conn,cur = utils.get_conn_cur()
 i=0
 while i < num:
         number = tmp[i]['number']
-        contract_name  = tmp[i]['contract_name']
-        name = tmp[i]['name']
-        address = tmp[i]['address'] 
-        position_lat = tmp[i]['position_lat']
-        position_lng = tmp[i]['position_lng']
-        banking = tmp[i]['banking']
-        bonus = tmp[i]['bonus']
+        
+        
+       
         bike_stands = tmp[i]['bike_stands']
         available_bike_stands = tmp[i]['available_bike_stands']
         available_bikes = tmp[i]['available_bikes']
-        status = tmp[i]['status'] 
+    
         last_update = tmp[i]['last_update'] 
         
         value1 = (number, contract_name, name, address, position_lat, position_lng, banking, bonus, bike_stands, available_bikes, status)
